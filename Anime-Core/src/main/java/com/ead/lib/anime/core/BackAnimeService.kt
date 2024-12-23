dc204e2fd69859c2def53f06e555ac641fd2a463
darkryh <alex_torres-xc@hotmail.com>
2024-12-23 17:07:06 -0500

    Object service

--- Anime-Core/src/main/java/com/ead/lib/anime/core/BackAnimeService.kt
+++ Anime-Core/src/main/java/com/ead/lib/anime/core/BackAnimeService.kt
@@ -2,11 +2,32 @@
 
 package com.ead.lib.anime.core
 
-import com.ead.lib.anime.core.core.service.Service
+import com.ead.lib.anime.core.core.Supplier
 
 object BackAnimeService {
 
-    lateinit var service : Service
+    private var _supplier : Supplier? = null
+    private val supplier : Supplier get() = _supplier ?: throw IllegalStateException("Config not initialized")
 
-    suspend fun getHome() = service.getHome()
+    fun onSupplier(supplier: Supplier) { _supplier = supplier }
+
+    suspend fun getHome() = supplier.baseService.getHome()
+
+    suspend fun getHomeDub() = supplier.dubService.getHome()
+
+    suspend fun getSearch(query : String) = supplier.baseService.getSearch(query)
+
+    suspend fun getSearchDub(query : String) = supplier.dubService.getSearch(query)
+
+    suspend fun getAnimeInfo(seo : String) = supplier.baseService.getAnimeInfo(seo)
+
+    suspend fun getAnimeInfoDub(seo : String) = supplier.dubService.getAnimeInfo(seo)
+
+    suspend fun getEpisodes(seo : String) = supplier.baseService.getEpisodes(seo)
+
+    suspend fun getEpisodesDub(seo : String) = supplier.dubService.getEpisodes(seo)
+
+    suspend fun getWatchInfoEpisode(seo : String) = supplier.baseService.getWatchInfoEpisode(seo)
+
+    suspend fun getWatchInfoEpisodeDub(seo : String) = supplier.dubService.getWatchInfoEpisode(seo)
 }
