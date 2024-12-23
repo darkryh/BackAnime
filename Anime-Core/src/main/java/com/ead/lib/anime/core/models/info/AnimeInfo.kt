c608a0666f8c54ad73014a69f792c3e1636fd27f
darkryh <alex_torres-xc@hotmail.com>
2024-12-23 17:06:30 -0500

    AnimeInfo data class

--- Anime-Core/src/main/java/com/ead/lib/anime/core/models/info/AnimeInfo.kt
+++ Anime-Core/src/main/java/com/ead/lib/anime/core/models/info/AnimeInfo.kt
@@ -1,13 +1,14 @@
 package com.ead.lib.anime.core.models.info
 
 data class AnimeInfo(
+    val seo : String,
     val title : String,
     val alternativeTitle : String?,
     val status : String,
     val coverImage : String,
     val profileImage : String?,
     val episodes : Int?,
-    val release : String,
-    val synopsis : String,
+    val release : String?,
+    val synopsis : String?,
     val genres : List<String>
 )
