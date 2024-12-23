7c059f24bfdd9666404340e0da879de3ed83e7ea
darkryh <alex_torres-xc@hotmail.com>
2024-12-23 17:08:51 -0500

    structure

--- Anime-Core/src/main/java/com/ead/lib/anime/core/models/structure/player/PlayerStructure.kt
+++ Anime-Core/src/main/java/com/ead/lib/anime/core/models/structure/player/PlayerStructure.kt
@@ -2,6 +2,5 @@ package com.ead.lib.anime.core.models.structure.player
 
 data class PlayerStructure(
     val optionsClassList : String,
-    val optionAttribute : String,
-    val downloadsClassList : String
+    val downloadsClassList : String?
 )
